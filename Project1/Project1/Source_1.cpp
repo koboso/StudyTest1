b8eaffa8cf477162cc9fd9fb04ab1edce53ad72f
koboso <hellowordc@gmail.com>
2021-03-23 22:25:16 +0900

    스터디 1 구구단 출력하기

--- Project1/Project1/Source_1.cpp
+++ Project1/Project1/Source_1.cpp
@@ -3,12 +3,21 @@
 using namespace std;
 
 void main() {
-	for (int i = 0; i < 5; i++) {
-		for (int j = 0; j <= i; j++) {
-			printf("*");
-		}
-		printf("\n");
-	}
+// 구구단 5단 출력하세요.
+	printf("1- 9 까지 수를 입력하세요 .\n");
+
+	int input;
+	scanf_s("%d", &input);
+
+	printf("%d * %d = %d\n", input, 1, input * 1);
+	printf("%d * %d = %d\n", input, 2, input * 2);
+	printf("%d * %d = %d\n", input, 3, input * 3);
+	printf("%d * %d = %d\n", input, 4, input * 4);
+	printf("%d * %d = %d\n", input, 5, input * 5);
+	printf("%d * %d = %d\n", input, 6, input * 6);
+	printf("%d * %d = %d\n", input, 7, input * 7);
+	printf("%d * %d = %d\n", input, 8, input * 8);
+	printf("%d * %d = %d\n", input, 9, input * 9);
 
 	fgetc(stdin);
 }
