beaca9b0b20b7451bf80e46f3bc28eba13901a32
koboso <hellowordc@gmail.com>
2021-03-23 22:06:44 +0900

    ㅇ

--- Project1/Project1/Source_1.cpp
+++ Project1/Project1/Source_1.cpp
@@ -3,7 +3,12 @@
 using namespace std;
 
 void main() {
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j <= i; j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
 
-	printf("Hello World");
 	fgetc(stdin);
 }
